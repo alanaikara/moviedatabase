6d85f859bab5c84e6ed0f39de9da305aae201911
alanaikara <alanantonyaikara@gmail.com>
2019-08-30 18:51:41 +0530

    php page to display the movie databse

---
+++ select.php
@@ -0,0 +1,55 @@
+ <?php  
+ $conn = mysqli_connect("localhost", "root", "", "moviedatabase");  
+ $output = '';  
+ $sql = "SELECT * FROM movie ORDER BY id DESC";  
+ $result = mysqli_query($conn, $sql);  
+ $output .= '  
+      <div align="center">  
+           <table border="1" bordercolor="#00CCCC">  
+                <tr>  
+                     <th width="10%">Id</th>  
+                     <th width="40%">Movie Name</th>  
+                     <th width="40%">Actorslist</th>  
+                     <th width="10%">rating</th> 
+                     <th width="40%">genre</th>  
+                     <th width="40%">year</th>  
+                     <th width="10%">image</th>  
+                     <th width="10%">options</th> 
+                </tr>';  
+ if(mysqli_num_rows($result) > 0)  
+ {  
+      while($row = mysqli_fetch_array($result))  
+      {  
+           $output .= '  
+                <tr>  
+                     <td>'.$row["id"].'</td>  
+                     <td class="movie_name" data-id1="'.$row["id"].'" >'.$row["name"].'</td>  
+                     <td class="actors_name" data-id2="'.$row["id"].'" >'.$row["actorslist"].'</td>  
+                     <td class="movie_rating" data-id3="'.$row["id"].'" >'.$row["rating"].'</td>  
+                     <td class="movie_genre" data-id4="'.$row["id"].'" >'.$row["genre"].'</td>  
+                     <td class="movie_year" data-id5="'.$row["id"].'" >'.$row["year"].'</td>  
+                     <td class="movie_image" data-id6="'.$row["id"].'" >'."<img src=\" " .$row["image"]."\" height=30px width=20px >".'</td> 
+                     <td><button type="button" name="delete_btn" data-id7="'.$row["id"].'" id="delete">Delete</button></td>  
+                     <td><button type="button" name="edit_btn" data-id8="'.$row["id"].'" id="edit">Edit</button></td>  
+
+           ';  
+      }  
+     //  $output .= '  
+     //       <tr>  
+     //            <td></td>  
+     //            <td id="first_name" contenteditable></td>  
+     //            <td id="last_name" contenteditable></td>  
+     //            <td><button type="button" name="add" id="add">Add</button></td>  
+     //       </tr>  
+     //  ';  
+ }  
+ else  
+ {  
+      $output .= '<tr>  
+                          <td colspan="4">Data not Found</td>  
+                     </tr>';  
+ }  
+ $output .= '</table>  
+      </div>';  
+ echo $output;  
+ ?>  
